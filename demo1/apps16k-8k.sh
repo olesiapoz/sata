4d0fd83c1e1ebb344842b5e37c4e6c89ee6a5b72
olesiapoz <32431445+olesiapoz@users.noreply.github.com>
2023-12-23 12:34:08 +0200

    Demo1 app added

---
+++ demo1/apps16k-8k.sh
@@ -0,0 +1,14 @@
+sudo mvn clean install -P docker
+wait
+
+label='16k-8k'
+
+echo "docker file build"
+sudo docker image tag  olesiapoz/demo1:latest olesiapoz/demo1:${label}
+sudo docker push olesiapoz/demo1:${label}
+
+#sudo docker push olesiapoz/demo1:latest
+wait
+
+echo "docker file build and pushed"
+
